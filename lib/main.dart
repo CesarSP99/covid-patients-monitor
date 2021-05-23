ea093053742e6976775a900cfa15b788ddf80214
CesarSP99 <cesarandres00@gmail.com>
2021-05-22 20:24:09 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Monitor',
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Monitor'),
+      ),
+      body: Center(
+        child: Container(
+          child: Text(''),
+        ),
+      ),
+    );
+  }
+}
